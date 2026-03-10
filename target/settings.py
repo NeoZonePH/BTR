163607a8f95942c7b8bc7e5cc171a946f1f7a5da
Mac User <christian.ragus1989@gmail.com>
2026-03-10 18:02:09 +0800

    Fixed the static files in the settings.py

--- target/settings.py
+++ target/settings.py
@@ -123,8 +123,8 @@ USE_TZ = True  # Store UTC in DB; ManilaTimezoneMiddleware activates Asia/Manila
 
 # Static files
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files
 MEDIA_URL = '/media/'
