d0998c6bd12b42283b6a3bcd5deab89e16570cbc
Christian <christian.ragus1989@gmail.com>
2026-03-10 12:03:52 +0000

    Fix the trusted origins in the settings.py

--- target/settings.py
+++ target/settings.py
@@ -122,9 +122,13 @@ USE_I18N = True
 USE_TZ = True  # Store UTC in DB; ManilaTimezoneMiddleware activates Asia/Manila per request
 
 # Static files
-STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Media files
 MEDIA_URL = '/media/'
@@ -181,3 +185,7 @@ CHANNEL_LAYERS = {
     },
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://10rcdgrescompa.com',
+    'https://www.10rcdgrescompa.com',
+]
